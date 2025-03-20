1eaf42075fe94545d93f3c5071f24fa73f0c6b97
StefanCirkovic <stefandjordje1508@gmail.com>
2025-03-20 10:00:59 +0100

    Update domaci_2.php

--- domaci_2.php
+++ domaci_2.php
@@ -11,14 +11,14 @@
 <html lang="en">
 <head>
    
-    <title> <?= $headline; ?></title>
+    <title> <?= $headline ?></title>
 </head>
 <body>
-    <h1> <?=  $headline; ?> </h1>
+    <h1> <?=  $headline ?> </h1>
     <nav> 
-        <a> <?= $navigation[0]; ?></a>
-        <a> <?= $navigation[1]; ?></a>
-        <a> <?= $navigation[2]; ?></a>
+        <a href="#"> <?= $navigation[0] ?></a>
+        <a href="#"> <?= $navigation[1] ?></a>
+        <a href="#"> <?= $navigation[2] ?></a>
     </nav>
     
 </body>
